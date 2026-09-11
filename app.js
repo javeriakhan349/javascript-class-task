de1ff7251ecc8aa07121430911a5d8e40db4f183
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-11 16:59:34 +0500

    Arrays

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 var studentsarray = [];
 .console.log(studentsarray);
 studentsarray.push = ["Alisha" , "Javeria"]
+
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+let studentNames = [];
+console.log(typeof studentNames); 
+
