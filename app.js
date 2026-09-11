c999b001c310a7fb333e11a81cbaed3f8b6be6d8
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-11 17:08:11 +0500

    Arrays

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1. Declare an empty array using JS literal notation to store student names in future.
 var studentsarray = [];
-.console.log(studentsarray);
+console.log(studentsarray);
 studentsarray.push = ["Alisha" , "Javeria"]
 
 
