84c5286d31652d1e12835a7d7cf0edc28e4a1481
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-22 17:23:46 +0500

    case sensitive

--- Class-5/app.js
+++ Class-5/app.js
@@ -16,6 +16,7 @@ for( i=1 ; i<=5 ; i++ )
     document.write("<br>")
 }
 
+
 document.write("<br><br><br>")
 // for( i=1 ; i<=5 ; i++ )
 // {
@@ -24,3 +25,35 @@ document.write("<br><br><br>")
 //     }
 //     document.write("<br>")
 // }
+
+
+//case sensitive
+//LOWER CASE
+var cities = [ "multan" , "karachi" , "islamabad" , "rawalpindi" ,"lahore" , "hyderabad"]
+var userCity = prompt("Enter your city name")
+var flag ="false"
+for( i=0 ; i<cities.length ; i++){
+    if ( userCity.toLowerCase() === cities[i]){
+        flag = "true"
+        console.log(`You are eligible`);
+    }
+}
+if( flag === "false"){
+    console.log(`You are not eligible`);
+}
+
+
+//UPPER CASE
+var fruits = [ "APPLE" ,"BANANA" , "KIWI" ,"STRAWBERRY", "WATERMELON"]
+var flag ="false"
+var userFruit = prompt("Check whether the fruit is avaliable or not")
+for ( j=0 ; j<fruits.length ; j++)
+{
+    if ( userFruit.toUpperCase() === fruits[j]){
+        flag = "true"
+        console.log("This fruit is avaliable  "+userFruit.toUpperCase());
+    }
+}
+if(flag === "false"){
+    console.log("This fruit is not avaliable  "+userFruit.toLowerCase());
+}
