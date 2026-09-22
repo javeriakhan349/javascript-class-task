7f2596cb24dab4f1e9ce6518188ff97e23a6d80b
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-22 16:50:09 +0500

    nested loops

---
+++ Class-5/app.js
@@ -0,0 +1,16 @@
+// for( i=1 ; i<=5 ; i++ )
+// {
+//     for( j=1 ; j<=5-i ; j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+document.write("<br><br><br>")
+for( i=1 ; i<=5 ; i++ )
+{
+    for( j=5 ; j>=1 ; j--){
+        document.write("*")
+    }
+    document.write("<br>")
+}
