8e8ef84b8e30cb51b5ea710d3cdd33a55bc8d4f0
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-22 16:55:50 +0500

    nested

--- Class-5/app.js
+++ Class-5/app.js
@@ -1,6 +1,16 @@
+//Nested Loops
+//pyramid pattern
 for( i=1 ; i<=5 ; i++ )
 {
     for( j=1 ; j<=5-i ; j++){
+        document.write("&nbsp;&nbsp;")
+    }
+    for( k=1 ; k<=i ; k++)
+    {
+        document.write("*")
+    }
+    for( l=2 ; l<=i ; l++)
+    {
         document.write("*")
     }
     document.write("<br>")
